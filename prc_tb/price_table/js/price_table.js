c3692ba46cf7aacad19687403b8193af66561b44
keple <smw195@gmail.com>
2020-02-16 19:50:14 +0900

    first commit

---
+++ prc_tb/price_table/js/price_table.js
@@ -0,0 +1,20 @@
+var jquery = require("jquery");
+require("bootstrap");
+require('../css/price_table.css');
+
+var splReceipt = (function(global){
+    var globalPage = global;
+
+    splReceipt.prototype.draw = function(){
+
+    }
+    splReceipt.prototype.config = function(dataArray){
+
+    }
+
+})(window);
+
+
+module.exports = splReceipt;
+
+
